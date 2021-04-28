1726b14836d51e8d361104f8441480bb6a72c35c
juanf <jfruizg@unbosque.edu.co>
2021-04-27 20:49:17 -0500

    Script - abrirarchivo

---
+++ Script/script.js
@@ -0,0 +1,17 @@
+const realFileBtn = document.getElementById("real-file"); //
+const customBtn = document.getElementById("botonSubmit");
+const customTxt = document.getElementById("etiquetaBoton");
+
+customBtn.addEventListener("click", function() {
+  realFileBtn.click();
+});
+
+realFileBtn.addEventListener("change", function() {
+  if (realFileBtn.value) {
+    customTxt.innerHTML = realFileBtn.value.match(
+      /[\/\\]([\w\d\s\.\-\(\)]+)$/
+    )[1];
+  } else {
+    customTxt.innerHTML = "No file chosen, yet.";
+  }
+});
